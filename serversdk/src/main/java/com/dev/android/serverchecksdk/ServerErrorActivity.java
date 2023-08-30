16a171e6acf7263341ff783455413a03cc605160
solutioninformatics <143509818+solutioninformatics@users.noreply.github.com>
2023-08-30 14:46:21 +0300

    Update ServerErrorActivity.java

--- serversdk/src/main/java/com/dev/android/serverchecksdk/ServerErrorActivity.java
+++ serversdk/src/main/java/com/dev/android/serverchecksdk/ServerErrorActivity.java
@@ -10,28 +10,26 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.dev.android.serverchecksdk.databinding.ActivityServerErrorBinding;
 import android.util.Base64;
-import com.dev.android.serverchecksdk.R;
 
 public class ServerErrorActivity extends AppCompatActivity {
 
+    private ActivityServerErrorBinding binding;
     private WebView webView;
-    private ProgressBar progressBar;
-    private TextView textView;
     CookieManager cookieManager = CookieManager.getInstance();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_server_error);
+        binding = ActivityServerErrorBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        webView = findViewById(R.id.webview);
-        progressBar = findViewById(R.id.progressBar);
-        textView = findViewById(R.id.textView);
+        webView = binding.webview;
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -46,23 +44,24 @@ public class ServerErrorActivity extends AppCompatActivity {
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
                 cookieManager.flush();
-                progressBar.setVisibility(View.VISIBLE);
-                textView.setVisibility(View.GONE);
+                binding.progressBar.setVisibility(View.VISIBLE);
+                binding.textView.setVisibility(View.GONE);
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 cookieManager.removeAllCookies(null);
-                progressBar.setVisibility(View.GONE);
-                textView.setVisibility(View.GONE);
+                binding.progressBar.setVisibility(View.GONE);
+                binding.textView.setVisibility(View.GONE);
                 super.onPageFinished(view, url);
+
             }
 
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 super.onReceivedError(view, request, error);
-                progressBar.setVisibility(View.GONE);
-                textView.setVisibility(View.VISIBLE);
+                binding.progressBar.setVisibility(View.GONE);
+                binding.textView.setVisibility(View.VISIBLE);
                 // This method is called when there's an error loading a web page
             }
         });
