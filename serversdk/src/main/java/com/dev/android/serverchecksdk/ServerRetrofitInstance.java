9e07c7c3155d3cad75fa9f71e66247f21c9c0dc5
solutioninformatics <143509818+solutioninformatics@users.noreply.github.com>
2023-08-30 10:01:39 +0300

    Update ServerRetrofitInstance.java

--- serversdk/src/main/java/com/dev/android/serverchecksdk/ServerRetrofitInstance.java
+++ serversdk/src/main/java/com/dev/android/serverchecksdk/ServerRetrofitInstance.java
@@ -14,6 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServerRetrofitInstance    {
     public static String userAgent = "";
+    public static String baseurl = "";
     private static Gson gson = new GsonBuilder()
             .setLenient()
             .create();
@@ -31,7 +32,7 @@ public class ServerRetrofitInstance    {
     }
 
     private static Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://solutioninformatics.com/")
+            .baseUrl(baseurl)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .client(getClient())
             .build();
